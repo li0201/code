2c61c492d9046bfdeaa529bdf5d8845bb7154639
li0201 <3516568463@qq.com>
2022-06-15 08:54:11 +0000

    qaq

--- test.cpp
+++ test.cpp
@@ -1,23 +1,10 @@
 #include <bits/stdc++.h>
 int x, y;
-const int N = 1e9;
-
 int main() {
     y = 0;
-    for (int i = 1; i <= N; ++i) {
+    for (int i = 1; i <= 1000000000; ++i) {
         x += y, y = (i & 1);
     }
     printf("%d\n", x);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
